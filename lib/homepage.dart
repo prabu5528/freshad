4bac8ba71e5df24e0daacf2635358d31eee087a4
prabu5528 <prabhakaran@inesssolutions.com>
2024-08-06 14:16:24 +0530

    flutter app

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,80 +1,110 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-import 'package:internet_connection_checker/internet_connection_checker.dart';
-import 'package:flutter/services.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:image_picker/image_picker.dart' as image_picker;
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+
+class WebViewMainPage extends StatefulWidget {
+  const WebViewMainPage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<WebViewMainPage> createState() => _WebViewMainPageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  late bool isDeviceConnected;
-  late Timer _timer;
-  bool _dialogShown = false; // Flag to track if the dialog has been shown
+class _WebViewMainPageState extends State<WebViewMainPage> {
+  late final WebViewController webViewController;
+  int loadingPercentage = 0;
 
   @override
   void initState() {
     super.initState();
-    isDeviceConnected = true; // Assuming device is initially connected
-    checkConnectivity();
-    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-      checkConnectivity();
-    });
+
+    webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(NavigationDelegate(
+        onPageStarted: (url) {
+          setState(() {
+            loadingPercentage = 0;
+          });
+        },
+        onProgress: (progress) {
+          setState(() {
+            loadingPercentage = progress;
+          });
+        },
+        onPageFinished: (url) {
+          setState(() {
+            loadingPercentage = 100;
+          });
+        },
+        onNavigationRequest: (navigationRequest) {
+          final String host = Uri.parse(navigationRequest.url).host;
+          if (host.contains("youtube.com")) {
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text("Navigation to $host is blocked")));
+            return NavigationDecision.prevent;
+          } else {
+            return NavigationDecision.navigate;
+          }
+        },
+      ))
+      ..loadRequest(
+        Uri.parse('https://freshad.mpntraders.net/'),
+      );
   }
 
-  @override
-  void dispose() {
-    _timer.cancel(); // Cancel the timer when the widget is disposed
-    super.dispose();
-  }
 
-  Future<void> checkConnectivity() async {
-    final isConnected = await InternetConnectionChecker().hasConnection;
-    setState(() {
-      isDeviceConnected = isConnected;
-    });
-    if (!isConnected && !_dialogShown) {
-      _dialogShown = true; // Set the flag to true to indicate the dialog has been shown
-      showDialogBox();
+  Future<bool> _onWillPop() async {
+    // Check if the WebView can go back
+    if (await webViewController.canGoBack()) {
+      webViewController.goBack();
+      return Future.value(false); // Prevent default back navigation
+    } else {
+      return (await showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Exit App'),
+          content: Text('Do you want to exit the app?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(false),
+              child: Text('No'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(true),
+              child: Text('Yes'),
+            ),
+          ],
+        ),
+      )) ?? false; // Use default exit behavior if no more history
     }
-  }
-
-  void showDialogBox() {
-    showDialog<String>(
-      context: context,
-      barrierDismissible: false, // Prevent dismissing the dialog by tapping outside
-      builder: (BuildContext context) => AlertDialog(
-        title: const Text('No Connection'),
-        content: const Text('Please check your internet connectivity'),
-        actions: <Widget>[
-          TextButton(
-            onPressed: () {
-              SystemNavigator.pop(); // Close the app
-            },
-            child: const Text('OK'),
-          ),
-        ],
-      ),
-    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return isDeviceConnected
-        ? WebviewScaffold(
-      url: 'https://freshad.mpntraders.net/',
-      withZoom: true,
-      withLocalStorage: true,
-      scrollBar: true,
-      withJavascript: true,
-    )
-        : Scaffold(
-      body: Center(
-        child: CircularProgressIndicator(),
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Freshad',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.green),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.green[100],
+        ),
+        body: Stack(
+          children: [
+            WebViewWidget(
+              controller: webViewController,
+            ),
+            if (loadingPercentage < 100)
+              Center(
+                child: CircularProgressIndicator(
+                  value: loadingPercentage / 100.0,
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
